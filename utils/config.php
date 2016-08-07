5cee283f2b2045cc8711c08548bb9b19c44bc84a
Luca8991 <luca.berte98@hotmail.it>
2016-08-07 19:43:13 +0200

    Update config.php

--- utils/config.php
+++ utils/config.php
@@ -7,9 +7,9 @@
  */
 
 $db_host = "localhost"; //Host address (most likely localhost)
-$db_name = "friday"; //Name of Database
-$db_user = "root"; //Name of database user
-$db_pass = "Ozzuseiunmito1"; //Password for database user
+$db_name = ""; //Name of Database
+$db_user = ""; //Name of database user
+$db_pass = ""; //Password for database user
 
 /* Create a new mysqli object with database connection parameters */
 $mysqli = new mysqli($db_host, $db_user, $db_pass, $db_name);
